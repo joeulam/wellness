2ede97f3144bb19df5e90a1b57bfeee0e8dde5e6
joeulam <joeulam345@gmail.com>
2022-03-09 23:09:10 -0500

    First attempt?

--- Main.java
+++ Main.java
@@ -1,1 +1,15 @@
-//start
+import java.util.Scanner; 
+
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        Scanner rating = new Scanner(System.in);
+        System.out.println("On a scale of 1-10 how are you feeling today?");
+        int userName = rating.nextInt();
+        System.out.print(Scale.ratingcal(userName));
+    }
+
+}
---
+++ Scale.java
@@ -0,0 +1,18 @@
+public class Scale {
+    
+
+    public static String ratingcal(int range)
+    {
+        if(range <= 3)
+        {
+            return "Oh no what happened?";
+        }
+        else if (range >= 4 && range <= 7)
+        {
+            return "Oh why was it medicore?";
+        }
+        else{
+            return "WOOO did something special happen?";
+        }
+    }
+}
