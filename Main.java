05f4e8a0e92a154fff18d10bf44ffe747f962668
Joey Lam <joeulam345@gmail.com>
2022-03-17 00:52:39 -0400

    Comments

--- Main.java
+++ Main.java
@@ -12,25 +12,29 @@ public class Main {
        
 
         try{
+            // DRIVER/CONNECTOR SET UP
             Class.forName("com.mysql.cj.jdbc.Driver");
             long millis=System.currentTimeMillis();  
             java.sql.Date date =new java.sql.Date(millis);
             String add = "jdbc:mysql://localhost:3306/app";
             String user = "joeulam";
             String pass = "jl308257";
+
+            //AUTHORIZATION & WHAT DATA TO INSORT
             Connection conn = DriverManager.getConnection(add,user,pass);
             PreparedStatement pstmt = conn.prepareStatement("INSERT INTO `data`(dates,scale) VALUES (?, ?)");
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            Scanner myObj = new Scanner(System.in);
+
+            Scanner myObj = new Scanner(System.in); //Open scanner class
             System.out.println("On a scale of 1-10 how are you feeling today?");
-            int rating = myObj.nextInt(); 
-            myObj.close(); 
-            System.out.println(Scale.ratingcal(rating));
-                pstmt.setDate(1, date);
-                pstmt.setInt(2,rating);
-                pstmt.executeUpdate();
+            int rating = myObj.nextInt(); //Read user response
+            myObj.close(); //close scanner
+
+            System.out.println(Scale.ratingcal(rating)); //return reponse
+                pstmt.setDate(1, date);//inserts date
+                pstmt.setInt(2,rating);//inserts reponse
+                pstmt.executeUpdate();//sends it to the database
                 System.out.println(" Inserted");
-                conn.close();
+                conn.close();//close database
             }
             catch (Exception e) {
                 System.err.println("Message: " + e.getMessage());
