b0c0b8989eb8cf9c024c4f94704e0188e3cf32fe
joeulam <joeulam345@gmail.com>
2022-03-23 20:15:23 -0400

    Fml

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
+
 import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.sql.* ;
+import java.io.Reader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
 
 public class Main {
 
@@ -13,12 +17,35 @@ public class Main {
             long millis=System.currentTimeMillis();  
             java.sql.Date date =new java.sql.Date(millis);
             String add = "jdbc:mysql://localhost:3306/app";
-            String user = "joeulam";
-            String pass = "jl308257";
+            Scanner username = new Scanner(System.in);
+            System.out.println("Type your username");
+            String logins = username.nextLine(); //Read user response
+            System.out.println("Enter your password");
+            String password = username.nextLine();
+            username.close();
+            Connection con = DriverManager.getConnection(add,"checker","guest");
+            PreparedStatement ps = con.prepareStatement("CREATE TABLE IF NOT EXISTS "+logins+"() ADD COLUMN `dates` DATETIME NOT NULL FIRST,ADD PRIMARY KEY (`dates`) ADD COLUMN `scale` INT NOT NULL AFTER `dates`, ADD COLUMN `reponse` VARCHAR(225) NULL AFTER `scale`,ADD PRIMARY KEY (`dates`, `scale`);");
+            
+            ps.executeQuery();
+
+            /*if(!(ps.executeQuery().equals(logins)))
+           {
+            Connection cons = DriverManager.getConnection(add,"checker","guest");
+            PreparedStatement tablemaker = cons.prepareStatement("CREATE TABLE `app`."+logins+"() ADD COLUMN `dates` DATETIME NOT NULL FIRST,ADD PRIMARY KEY (`dates`) ADD COLUMN `scale` INT NOT NULL AFTER `dates`, ADD COLUMN `reponse` VARCHAR(225) NULL AFTER `scale`,ADD PRIMARY KEY (`dates`, `scale`);");
+            tablemaker.executeQuery ();
+           }*/
+            
+            String user = logins;
+            String pass = password;
 
             //AUTHORIZATION & WHAT DATA TO INSORT
             Connection conn = DriverManager.getConnection(add,user,pass);
+
+
             PreparedStatement pstmt = conn.prepareStatement("INSERT INTO `data`(dates,scale,reponse) VALUES (?, ?, ?)");
+
+
+
 
             Scanner myObj = new Scanner(System.in); //Open scanner class
             System.out.println("On a scale of 1-10 how are you feeling today?");
