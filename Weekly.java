9e69be465e53152cd61cd2d4d56fecca59ea110f
joeulam <joeulam345@gmail.com>
2022-04-03 19:51:29 -0400

    Fixed it !!! working on 7 day average

--- Weekly.java
+++ Weekly.java
@@ -4,11 +4,17 @@ import java.sql.* ;
 public class Weekly{
     public static double ave(){
         double a = 0;
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        String add = "jdbc:mysql://localhost:3306/app";
-        Connection con = DriverManager.getConnection(add,Main.user);
-
-
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String add = "jdbc:mysql://localhost:3306/app";
+            Connection con = DriverManager.getConnection(add,Main.logins,Main.password);
+            con.prepareStatement("SELECT FROM '"+Main.logins+"'");
+        }
+        catch(Exception e){
+            System.err.println("Message: " + e.getMessage());
+        }
+        
         return a;
+        
     }
 }
