69003c8d54226aea04e010438b3b3858dce7b90c
Joey Lam <joeulam345@gmail.com>
2022-05-09 10:59:38 -0400

    Create rating.java

---
+++ rating.java
@@ -0,0 +1,12 @@
+public class rating {
+    String toberated;
+    public rating(String a)
+    {
+        toberated = a;
+    }
+
+    public void backcount()
+    {
+        if(toberated.equals())
+    }
+}
