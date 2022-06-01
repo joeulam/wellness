b1ce60a4e374d0b5aa34a63c1b7d0c78eb70d0a2
Joey Lam <joeulam345@gmail.com>
2022-05-31 22:48:47 -0400

    Gui created

--- login.py
+++ login.py
@@ -4,14 +4,34 @@ from PyQt6.QtWidgets import QApplication
 from PyQt6.QtWidgets import QHBoxLayout
 from PyQt6.QtWidgets import QPushButton
 from PyQt6.QtWidgets import QWidget
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import *
+from PyQt6.QtCore import *
 
-app = QApplication(sys.argv)
-window = QWidget()
-window.setWindowTitle('QHBoxLayout')
-layout = QHBoxLayout()
-layout.addWidget(QPushButton('Left'))
-layout.addWidget(QPushButton('Center'))
-layout.addWidget(QPushButton('Right'))
-window.setLayout(layout)
-window.show()
-sys.exit(app.exec_())
+
+class Dialog(QDialog):
+    def __init__(self,parent=None):
+        super().__init__(parent)
+        self.setWindowTitle('Login page')
+        dlgLayout = QVBoxLayout()
+        formLayout = QFormLayout()
+        l1 = QLineEdit()
+        l1.setEchoMode(QLineEdit.EchoMode.Password)
+        l2 = QLineEdit()
+        formLayout.addRow('Username',l2)
+        formLayout.addRow('Password',l1)
+        loginbt = QPushButton('Login')
+        formLayout.addRow(loginbt)
+        loginbt.clicked.connect(login_info)
+        dlgLayout.addLayout(formLayout)        
+        self.setLayout(dlgLayout)
+    
+   
+def login_info():
+    print("p")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    dlg = Dialog()
+    dlg.show()
+    sys.exit(app.exec())
